609e292d3729ef6f6f4fa5966a0b51428d552b10
Ivan <nazgoool1@gmail.com>
2022-11-01 15:57:31 +0400

    create worker sub

--- py_pubsub/py_pubsub/pub_robot_move_control.py
+++ py_pubsub/py_pubsub/pub_robot_move_control.py
@@ -1,35 +1,64 @@
+# import sys
+# import rclpy
+# from rclpy.node import Node
+# from geometry_msgs.msg import Twist
+
+# class Robot_control(Node):
+
+#     def __init__(self):
+#         super().__init__("robot_control_node")
+#         self.cmd_vel_pub_ = self.create_publisher(Twist,'robot_first/cmd_vel', 1 )
+#         self.timer_ = self.create_timer(0.5, self.send_velosity_comand)
+#         self.get_logger().info("start --> create new control")
+
+#     def send_velosity_comand(self):
+#         msg = Twist()
+#         msg.linear.x = float(sys.argv[1])
+#         msg.angular.z = float(sys.argv[2])
+#         self.cmd_vel_pub_.publish(msg)
+
+
+# def main(args = Node):
+#     rclpy.init(args=args)
+#     node = Robot_control()
+#     rclpy.spin(node)
+#     rclpy.shutdown()
+
+# if __name__=='__main__':
+#     main()
+
+
+
+import sys
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
-class Robot_control(Node):
+class MinimalPublisher(Node):
 
     def __init__(self):
-        super().__init__("robot_control_node")
-        self.cmd_vel_pub_ = self.create_publisher(Twist,"/turtle1/cmd_vel", 10 )
-        self.timer_ = self.create_timer(0.5, self.send_velosity_comand)
-        #self.get_logger().info("start --> create new control")
+        super().__init__('py_topic_publisher_spiral')
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 1)
+        timer_period = 0.5  # seconds
+        self.i = 0.0
+        self.timer_ = self.create_timer(timer_period, self.publish_message)
 
-    def send_velosity_comand(self):
-        msg = Twist()
-        msg.linear.x = 2.5
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
+    def publish_message(self):
+        message = Twist()
+        x = 3.5
+        z = 0.0
+        message.linear.x = float(x)
+        message.angular.z = float(z)
+        self.get_logger().info('Sending - Linear Velocity : %f, Angular Velocity : %f' % (message.linear.x, message.angular.z))
+        self.publisher_.publish(message)
+        self.i += 0.1
 
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = 1.2
-        self.cmd_vel_pub_.publish(msg)
-
-
-def main(args = Node):
+def main(args=None):
     rclpy.init(args=args)
-    node = Robot_control()
-    rclpy.spin(node)
+    minimal_publisher = MinimalPublisher()
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
     rclpy.shutdown()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
-
